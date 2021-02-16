eebeefb8b3006997ce42240df9056e255efd6434
AllanKamimura <65671989+AllanKamimura@users.noreply.github.com>
2021-02-16 18:07:07 -0300

    add logos and AddImage

--- src/Layout/Footer/Footer.jsx
+++ src/Layout/Footer/Footer.jsx
@@ -1,5 +1,48 @@
 import { Col, Container, Row } from "react-bootstrap";
-import logo from "../../logo.svg";
+
+const logos = {
+  "cs": {
+    "path": "../../logo_cs.png",
+    "width": 160,
+    "height": 90,
+    "alt": "cs_logo"
+  },
+  "ras": {
+    "path": "../../logo_ras.png",
+    "width": 90,
+    "height":90,
+    "alt": "ras_logo"
+  },
+  "ieee": {
+    "path": "../../logo_ieee.png",
+    "width": 160,
+    "height": 90,
+    "alt": "ieee_logo"
+  },
+  "wie": {
+    "path": "../../logo_wie.png",
+    "width": 90,
+    "height": 90,
+    "alt": "wie_logo"
+  },
+  "pes": {
+    "path": "../../logo_pes.png",
+    "width": 150,
+    "height": 90,
+    "alt": "pes_logo"
+}}
+
+function AddImage(nome){
+  return(
+    <img
+    src = {logos[nome].path}
+    width = {logos[nome].width}  
+    height = {logos[nome].height}
+    className="d-inline-block align-top"
+    alt = {logos[nome].alt}
+  />
+  )
+}
 
 function Footer() {
   return (
@@ -7,50 +50,23 @@ function Footer() {
       <Container>
         <Row>Contato | O Ramo | Endereço</Row>
         <Row>
+          <span class="text-muted">.</span>
+        </Row>
+        <Row>
           <Col md="3">
-            <img
-              src={logo}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
+            {AddImage("cs")}
           </Col>
           <Col md="2">
-            <img
-              src={logo}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
+            {AddImage("wie")}
           </Col>
           <Col md="2">
-            <img
-              src={logo}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
+            {AddImage("ieee")}
           </Col>
           <Col md="2">
-            <img
-              src={logo}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
+            {AddImage("ras")}
           </Col>
           <Col md="3">
-            <img
-              src={logo}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-              alt="React Bootstrap logo"
-            />
+            {AddImage("pes")}
           </Col>
         </Row>
         <Row>
