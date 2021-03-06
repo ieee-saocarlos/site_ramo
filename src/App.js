8d6d6e5568ee4e55deaa7b43a66ededa4d5da907
PedroRibasSerras <pedrofhp@usp.br>
2021-03-06 20:47:57 -0300

    Close #6

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import Header from './Layout/Header/Header';
-import './App.css';
-import Footer from './Layout/Footer/Footer';
+import Header from "./Layout/Header/Header";
+import "./App.css";
+import Footer from "./Layout/Footer/Footer";
 // import Home from './Pages/Home/Home';
-import Branch from './Pages/Branch/Branch';
+import Branch from "./Pages/Branch/Branch";
+import Home from "./Pages/Home/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <Header></Header>
-      {/* <Home></Home> */}
-      <Branch></Branch>
-      <Footer></Footer>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header></Header>
+            <Home></Home>
+            <Footer></Footer>
+        </div>
+    );
 }
 
 export default App;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,70 +1,59 @@
 import React, { Component } from "react";
-import { Button, Card, Col, Container, Jumbotron, Row } from "react-bootstrap";
+import { Button, Col, Container, Jumbotron, Row } from "react-bootstrap";
+import { CardsList } from "../../components/CardsList";
+import news from "../../news.json";
 import CalendarProvider from "../../components/CalendarProvider";
 import logo from "../../logo.svg";
 
 export class Home extends Component {
-  static propTypes = {};
+    static propTypes = {};
 
-  render() {
-    return (
-      <div>
-        <Jumbotron>
-          <h1>Hello, world!</h1>
-          <p>
-            This is a simple hero unit, a simple jumbotron-style component for
-            calling extra attention to featured content or information.
-          </p>
-          <p>
-            <Button variant="primary">Learn more</Button>
-          </p>
-        </Jumbotron>
+    render() {
+        return (
+            <div>
+                <Jumbotron>
+                    <h1>Hello, world!</h1>
+                    <p>
+                        This is a simple hero unit, a simple jumbotron-style
+                        component for calling extra attention to featured
+                        content or information.
+                    </p>
+                    <p>
+                        <Button variant="primary">Learn more</Button>
+                    </p>
+                </Jumbotron>
 
-        <Container>
-          <Row>
-            <Col md="8">
-              <Col md="12">
-                <Card>
-                  <Row>
-                    <Col>
-                      <img alt="Card" src={logo} width="200" />
-                    </Col>
-                    <Col>
-                      <Card.Body>
-                        <Card.Text>
-                          This is a wider card with supporting text below as a
-                          natural lead-in to additional content.
-                        </Card.Text>
-                      </Card.Body>
-                    </Col>
-                  </Row>
-                </Card>
-              </Col>
-            </Col>
-            <Col md="4" as="aside">
-              <div className="p-3 mb-3 bg-light rounded">
-                <h4 className="font-italic">About</h4>
-                <p className="mb-0">
-                  Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras
-                  mattis consectetur purus sit amet fermentum. Aenean lacinia
-                  bibendum nulla sed consectetur.
-                </p>
-              </div>
-              <div className="p-3 mb-3 bg-light rounded">
-                <h4 className="font-italic">About</h4>
-                <p className="mb-0">
-                  Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras
-                  mattis consectetur purus sit amet fermentum. Aenean lacinia
-                  bibendum nulla sed consectetur.
-                </p>
-              </div>
-                <CalendarProvider />
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+                <Container>
+                    <Row>
+                        <Col md="8">
+                            <CardsList data={news} initNumberOfData={3} />
+                        </Col>
+                        <Col md="4" as="aside">
+                            <div className="p-3 mb-3 bg-light rounded">
+                                <h4 className="font-italic">About</h4>
+                                <p className="mb-0">
+                                    Etiam porta <em>sem malesuada magna</em>{" "}
+                                    mollis euismod. Cras mattis consectetur
+                                    purus sit amet fermentum. Aenean lacinia
+                                    bibendum nulla sed consectetur.
+                                </p>
+                            </div>
+                            <div className="p-3 mb-3 bg-light rounded">
+                                <h4 className="font-italic">About</h4>
+                                <p className="mb-0">
+                                    Etiam porta <em>sem malesuada magna</em>{" "}
+                                    mollis euismod. Cras mattis consectetur
+                                    purus sit amet fermentum. Aenean lacinia
+                                    bibendum nulla sed consectetur.
+                                </p>
+                            </div>
+                            <CalendarProvider />
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
 }
 
 export default Home;
