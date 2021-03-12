ff6bff09caf87f9a76051540df0aad290ff54b98
LuanSSouza <luan.souza27@hotmail.com>
2021-03-12 00:53:02 -0300

    updating classes names

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import Header from './Layout/Header/Header';
-import logo from './logo.svg';
 import './App.css';
 import Footer from './Layout/Footer/Footer';
 // import Home from './Pages/Home/Home';
-import About from './Pages/About/About';
+import Branch from './Pages/Branch/Branch';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       {/* <Home></Home> */}
-      <About></About>
+      <Branch></Branch>
       <Footer></Footer>
     </div>
   );
