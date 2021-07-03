588e09e30fb340adb0093717e19c1babf4b3a93d
Rafaela Bull <rafaelabull@usp.br>
2021-07-03 19:18:48 -0300

    Fix nome da pasta Components pt2

---
+++ src/Components/NewsCard/CardItem.jsx
@@ -0,0 +1,24 @@
+import { Card,Row,Col } from "react-bootstrap";
+
+export function CardItem(props){
+
+    return(
+        <Card className="mt-3">
+            <Row>
+                <Col className="mt-2 mb-2" sm="12" lg style={{overflow:"hidden"}}>
+                        <img alt="Card" src={"./newsImages/" + props.img} width="auto" height="200em" />
+                </Col>
+                <Col>
+                    <Card.Body>
+                        <Card.Title>
+                            {props.title}
+                        </Card.Title>
+                        <Card.Text>
+                        {props.description}
+                        </Card.Text>
+                    </Card.Body>
+                </Col>
+            </Row>
+        </Card>
+    )
+}
